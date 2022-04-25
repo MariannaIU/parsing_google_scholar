88594e492933b29c6ce2ba9dcd0941eba827384f
Marianna <marianka101@gmail.com>
2022-04-25 14:24:33 +0200

    save_to_csv added

---
+++ save_to_csv.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from google_scholar_organic_results import organic_results
+from google_scholar_cite_results import cite_results
+
+print("waiting for organic results to save..")
+pd.DataFrame(data=organic_results()).to_csv("google_scholar_organic_results.csv", encoding="utf-8", index=False)
+
+print("waiting for cite results to save..")
+pd.DataFrame(data=cite_results()).to_csv("google_scholar_citation_results.csv", encoding="utf-8", index=False)
